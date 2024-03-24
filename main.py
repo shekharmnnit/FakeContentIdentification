69f3209b41665ae0e27ff08760755699e4db2d22
shekharmnnit <shekhark221@gmail.com>
2024-03-24 11:15:31 -0400

    CORS error fix

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ from service.scraper import Scraper
 import pandas as pd
 from fastapi import FastAPI, Form
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 from service.news_classifier_service import NewsClassifierService
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:*",  # adjust to match the domain of your client app
+    "http://localhost:8000",  # adjust to match the domain of your client app
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
